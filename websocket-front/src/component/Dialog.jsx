65cbc1c1da7523b84224277971b6318a506fa46d
Jie ren Tian <110873204@qq.com>
2018-03-05 14:31:11 +0800

    bg add.

--- websocket-front/src/component/Dialog.jsx
+++ websocket-front/src/component/Dialog.jsx
@@ -3,11 +3,23 @@
  * Created by tianrenjie on 2018/3/5
  */
 import React, { PropTypes } from 'react';
+import { Row, Col, Card } from 'antd';
 
 class Dialog extends React.Component {
   static propTypes = {};
   render() {
-    return (<div>Home</div>);
+    return (
+      <div>
+        <Row>
+          <Col span={12} offset={6}>
+            <Card>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+          </Col>
+        </Row>
+      </div>);
   }
 }
 
