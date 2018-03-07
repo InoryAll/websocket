167ec66557eb438cbee668f2bcc0950d2e127646
Jie ren Tian <110873204@qq.com>
2018-03-07 16:55:25 +0800

    action updated.

--- websocket-front/src/action/action.js
+++ websocket-front/src/action/action.js
@@ -3,11 +3,32 @@
  * Created by tianrenjie on 2017/11/9
  */
 import { createAction } from 'redux-actions';
+import { message } from 'antd';
 import { ON_CONNECT, ON_DISCONNECT, SEND_MESSAGE } from '../constant/constant';
 
 export const onConnectAction = createAction(ON_CONNECT);
 export const sendMessageAction = createAction(SEND_MESSAGE);
 export const onDisconnectAction = createAction(ON_DISCONNECT);
+
+/**
+ * 连接到后台
+ * @param socketUtil
+ * @returns {function(*)}
+ */
+export function connectToServer(socketUtil) {
+  return (dispatch) => {
+    socketUtil.attachConnectEvent((msg) => {
+      console.log(msg);
+      if (msg.code === 1) {
+        dispatch(onConnectAction());
+        message.success('连接成功!');
+      } else {
+        message.error('连接失败!');
+      }
+    });
+  };
+}
+
 
 /**
  * 发送信息
@@ -28,5 +49,24 @@ export function sendMesasge() {
       console.log(e);
     });
     dispatch(sendMessageAction());
+  };
+}
+
+/**
+ * 从后台断开
+ * @param socketUtil
+ * @returns {function(*)}
+ */
+export function disConnectFromServer(socketUtil) {
+  return (dispatch) => {
+    socketUtil.attachDisconnectEvent((msg) => {
+      console.log(msg);
+      if (msg.code === 1) {
+        dispatch(onDisconnectAction());
+        message.success('断开成功!');
+      } else {
+        message.error('断开失败!');
+      }
+    });
   };
 }
